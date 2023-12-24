05172b6e01faf01fb20b13c041efee7c27b611bb
zslade <zoe.slade@gmail.com>
2023-12-24 21:35:47 +0000

    adding comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
+// to enable JSX in .js file
 import * as React from 'react'
 import Layout from '../components/layout'
 
+// fucntion that returns a JSX element
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
@@ -9,6 +11,9 @@ const IndexPage = () => {
   )
 }
 
+// gatsby Head component to apply metadata
 export const Head = () => <title>Home Page</title>
 
+/* Step 3: Export your component so it
+can be used by other parts of your app. */
 export default IndexPage
