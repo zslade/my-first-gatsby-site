4a3c57c0c59272ea0cac758b5a7c2b0a28b933b3
zslade <zoe.slade@gmail.com>
2023-12-28 19:54:17 +0000

    initialise mondrian article

---
+++ src/pages/mondrian.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const MondrianPage = () => {
+  return (
+    <Layout pageTitle="The ultimate abstraction?">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac augue eget velit vestibulum ultrices. Nulla facilisi. Fusce vel fermentum odio, et vulputate libero. Quisque ut justo non sem bibendum aliquam. Ut elementum, tortor at blandit ultrices, augue justo malesuada orci, eu fringilla orci eros nec velit. Nam at ex vel turpis posuere luctus. In hac habitasse platea dictumst. Phasellus non mauris nec est facilisis consequat. Maecenas convallis mi non augue ultricies, id consectetur erat tincidunt.
+          t sem. Quisque eu lectus eu urna interdum fermentum. Vivamus et fringilla justo, vel dictum dui.
+      </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac augue eget velit vestibulum ultrices. Nulla facilisi. Fusce vel fermentum odio, et vulputate libero. Quisque ut justo non sem bibendum aliquam. Ut elementum, tortor at blandit ultrices, augue justo malesuada orci, eu fringilla orci eros nec velit. Nam at ex vel turpis posuere luctus. In hac habitasse platea dictumst. Phasellus non mauris nec est facilisis consequat. Maecenas convallis mi non augue ultricies, id consectetur erat tincidunt.
+      </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac augue eget velit vestibulum ultrices. Nulla facilisi. Fusce vel fermentum odio, et vulputate libero. Quisque ut justo non sem bibendum aliquam. Ut elementum, tortor at blandit ultrices, augue justo malesuada orci, eu fringilla orci eros nec velit. Nam at ex vel turpis posuere luctus. In hac habitasse platea dictumst. Phasellus non mauris nec est facilisis consequat. Maecenas convallis mi non augue ultricies, id consectetur erat tincidunt.
+       </p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>The ultimate abstraction?</title>
+
+export default MondrianPage
