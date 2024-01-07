e85a9a6ef19ce3bef02648bc2d134532e14d245d
zslade <zoe.slade@gmail.com>
2024-01-07 22:34:39 +0000

    tweak about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,16 +13,14 @@ const AboutPage = () => {
       <h2>The graph</h2>
       <p>
         The graph on the home page acts as the blog's contents page.
-        The little circles (a.k.a. nodes) represent blog articles, colour-coded according to topic. Clicking on the article's title will take you to the article itself.
+        The little circles (a.k.a. <a href="https://en.wikipedia.org/wiki/Graph_theory#Definitions" style={{color: "#626262"}}>nodes</a>) represent blog articles, colour-coded according to topic. Clicking on the article's title will take you to the article itself.
         Articles are linked to one another through common themes. 
-        As the number of articles grows, I plan to introduce more interesting ways of linking and filtering them.
+        As the number of articles grows, I intend to introduce more interesting ways of linking and filtering them.
       </p>
       <h3>Why?</h3>
       <p>
-        {/* I chose to design the blog like this because it allows me to visually represent the interconnectedness of things.
-        I find it pretty fascinating how the same concepts crop up in seemingly disparate fields and 
-        how the same phenomena can be understood from many different perspecives.
-        (Maybe it's not all that profound and simply a reflection of how the human mind works, but I still find it pretty cool nonetheless.) */}
+      I designed my blog like this because it enables me a to visually convey the connections between the things I write about.
+      It also makes for a cool landing page and a fun graphic for people to play with.
       </p>
       <h3>How?</h3>
       <p>
@@ -30,31 +28,16 @@ const AboutPage = () => {
         the <a href="https://react.dev/" style={{color: "#626262"}}>React framework</a>. 
         I used <a href="https://observablehq.com" style={{color: "#626262"}}>Observable</a> to develop the graph on the home page before integrating with the Gatsby site.
         All source code for the blog is contained in <a href="https://github.com/zslade/graph-blog" style={{color: "#626262"}}>this GitHub repo</a>.
-        Feel free to report any bugs or share ideas for improvements via GitHub issues.
-      </p>
-      <h2>The site</h2>
-      <p>
-        As well as new articles, there are lots of ideas I have for extending the appearance and functionality of the website itself.
-        <br></br>
-        <br></br>
-
-        <b>Under development</b>
-        <ul>
-          <li>A "key" for article topics</li>
-          <li>Tooltips for article summary display on hover</li>
-          <li>Hyperlinked LinkedIn favicon for navigation bar</li>
-        </ul>
-      
-        <b>Future developments</b>
-        <ul>
-          <li>Optional "linked list" display for large number of nodes (especially important for mobiles)</li>
-          <li>Filtering options for users</li>
-          <li>NLP to generate links between articles</li>
-        </ul>
+        {/* Feel free to report any bugs or share ideas for improvements via GitHub issues. */}
       </p>
       <p>
+        This site is work in progress. 
+        Alongside publishing new articles, I will be making improvements to the look and functionality of the website itself, 
+        such as ensuring easy navigation as the number of articles grows.
+      </p>
+      {/* <p>
         I post about my latest blog articles on <a href="https://www.linkedin.com/in/zoe-slade/" style={{color: "#626262"}}>LinkedIn</a> if you'd like to stay up to date.
-      </p>
+      </p> */}
     </Layout>
   )
 }
