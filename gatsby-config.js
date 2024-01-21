8fd9e04abb6115c55b0114e6ad35795d1bbd0147
zslade <zoe.slade@gmail.com>
2024-01-21 20:30:47 +0000

    use gatsby image plugin and add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,12 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My First Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Zoe's Graph Blog`,
+    siteUrl: `https://www.zoesgraphblog.com`,
+    firstPublishedDate: `2024-01-12`
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
