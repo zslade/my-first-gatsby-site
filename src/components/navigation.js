6389e0d5211b99f89c1f750a78bb26bbe6cdb26a
zslade <zoe.slade@gmail.com>
2024-07-20 12:42:39 +0100

    position key

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,16 +2,20 @@ import * as React from 'react';
 // link is a prebuilt component maintained by gatsby
 import { Link } from 'gatsby';
 import {
+  navContainer,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  navKey,
+  navRight
 } from '../css/navigation.module.css';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className={navContainer}>
       <ul className={navLinks}>
         <li className={navLinkItem}>
           <Link to="/" className={navLinkText}>
@@ -28,11 +32,20 @@ const Navigation = () => {
             style={{
               color: `#0077B5`,
             }} 
-            href={`https://www.linkedin.com/in/zoe-slade/`} >
+            href={`https://www.linkedin.com/in/zoe-slade/`} 
+            target='_blank'
+          >
             <FontAwesomeIcon icon={faLinkedin}/>
           </a>
         </li>
-      </ul>
+        </ul>
+        <div className={navRight}>
+              <StaticImage
+                alt="Colour-coded key to blog post themes"
+                src="../images/graph_key.svg"
+                className={navKey}
+              />
+        </div>
     </nav>
   )
 }
