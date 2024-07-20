cd01d56584ef9cc97cf1ed40012dc4a83d6fbe21
zslade <zoe.slade@gmail.com>
2024-07-20 17:05:09 +0100

    Add colour to About Page title

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,41 @@ import { Link } from 'gatsby';
 import {
   container,
   heading,
+  color1,
+  color2,
+  color3,
+  color4,
+  color5
 } from '../css/layout.module.css';
 import { navLinkFooter} from '../css/navigation.module.css';
 import Navigation from './navigation';
 
 const Layout = ({ pageTitle, children }) => {
+  const isAboutPage = pageTitle.includes('About');
+
   return (
     <div className={container}>
       <Navigation />
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
+        <h1 className={heading}>
+        {isAboutPage ? (
+            <>
+              About{" "}
+              <span className={color1}>G</span>
+              <span className={color2}>r</span>
+              <span className={color3}>a</span>
+              <span className={color4}>p</span>
+              <span className={color5}>h</span>
+              {" "}
+              <span className={color1}>B</span>
+              <span className={color2}>l</span>
+              <span className={color3}>o</span>
+              <span className={color4}>g</span>
+            </>
+          ) : (
+            pageTitle
+          )}
+        </h1>
         {children}
       </main>
       <br></br>
